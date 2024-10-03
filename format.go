1a7f6881778628d403cd11967feb0d628fac5e42
Chrstopher Hunter <8398225+crhntr@users.noreply.github.com>
2024-10-03 13:54:34 -0700

    format go.mod

--- format.go
+++ format.go
@@ -6,11 +6,12 @@ import (
 	"go/format"
 	"path/filepath"
 
+	"golang.org/x/mod/modfile"
 	"golang.org/x/tools/txtar"
 )
 
 type Configuration struct {
-	SkipGo, SkipJSON bool
+	SkipGo, SkipJSON, SkipGoMod bool
 }
 
 func Archive(archive *txtar.Archive, config Configuration) error {
@@ -31,13 +32,22 @@ func File(file txtar.File, config Configuration) (txtar.File, error) {
 			return file, err
 		}
 		file.Data = out
-	}
-	if !config.SkipJSON && filepath.Ext(file.Name) == ".json" {
+	} else if !config.SkipJSON && filepath.Ext(file.Name) == ".json" {
 		var buf bytes.Buffer
 		if err := json.Indent(&buf, file.Data, "", "  "); err != nil {
 			return file, err
 		}
 		file.Data = buf.Bytes()
+	} else if !config.SkipGoMod && filepath.Base(file.Name) == "go.mod" {
+		modFile, err := modfile.Parse(file.Name, file.Data, nil)
+		if err != nil {
+			return file, err
+		}
+		buf, err := modFile.Format()
+		if err != nil {
+			return file, err
+		}
+		file.Data = buf
 	}
 	return file, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/crhntr/txtarfmt
 
 go 1.23.0
 
-require golang.org/x/tools v0.24.0 // indirect
+require (
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/tools v0.24.0 // indirect
+)
